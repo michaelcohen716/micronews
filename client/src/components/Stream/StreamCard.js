9c199779e0c774f3f6f78ed3194717dd60b9e1c4
michaelcohen716 <michaelcohen716@gmail.com>
2019-09-26 13:28:01 -0400

    stream card

--- client/src/components/Stream/StreamCard.js
+++ client/src/components/Stream/StreamCard.js
@@ -1,17 +1,32 @@
 import React, { useState } from "react";
+import upArrow from "../../assets/up-arrow.png";
+import downArrow from "../../assets/down-arrow.png";
 import "./Stream.css";
 
-function StreamCard(){
-    return (
-        <div className="d-flex stream-card my-3">
-            <div className="vote-slice">
-                {/* up/down */}
-            </div>
-            <div className="content-slice">
-                {/* content */}
-            </div>
+function StreamCard() {
+  return (
+    <div className="d-flex stream-card my-3">
+      <div className="vote-slice d-flex flex-column">
+        <div className="d-flex flex-column my-auto mx-auto">
+          <img src={upArrow} className="img-fluid arrow-img" />
+          <img src={downArrow} className="img-fluid arrow-img mt-3" />
         </div>
-    )
+      </div>
+      <div className="content-slice p-2 mx-2 d-flex flex-column">
+        <div className="d-flex content-header mt-2">
+          <div className="channel-title font-weight-bold">m/gaming</div>
+          <div className="timestamp ml-3">Posted by Creator472 2 hours ago</div>
+        </div>
+        <div className="content-title mt-4">
+          Counterstrike 17 is the best game ever invented
+        </div>
+        <div className="content-preview mt-4">
+          I first discovered Counterstrike when I was 12 years old. The gameplay
+          was...
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default StreamCard;
